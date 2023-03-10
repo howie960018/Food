c0e34f5d15b91438850fa083852cd56e3826c075
howie960018 <newhowie0427@gmail.com>
2023-03-10 19:05:15 +0800

    Initial Commit

---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by 曾浩儀 on 2023/3/10.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
